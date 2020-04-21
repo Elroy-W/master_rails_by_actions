85e96ab607608d30b95b618998929dffd5672e88
Rookie <1041743913@qq.com>
2020-04-22 05:12:33 +0800

    user register and log in/out

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(params.require(:user)
+        .permit(:email, :password, :password_confirmation))
+  
+      if @user.save
+        flash[:notice] = "Sign  Successfully, please log in"
+        redirect_to new_session_path
+      else
+        render action: :new
+      end
+    end
+  
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,31 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  attr_accessor :password, :password_confirmation
+
+  validates_presence_of :email, message: "Email address can not be blank!"
+  validates_format_of :email, message: "Email address is invalid!", 
+  with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+  if: proc { |user| !user.email.blank? }
+validates :email, uniqueness: true
+
+
+  validates_presence_of :password, message: "Password can not be blank！", 
+    if: :need_validate_password
+  validates_presence_of :password_confirmation, message: "Please confirm your password",
+    if: :need_validate_password
+  validates_confirmation_of :password, message: "Password does not match to confirm password",
+    if: :need_validate_password
+  validates_length_of :password, message: "Passwords must have at least 6 characters！", minimum: 6,
+    if: :need_validate_password
+
+      def username
+        self.email.split('@').first
+    end
+
+  private
+  def need_validate_password
+    self.new_record? || 
+      (!self.password.nil? || !self.password_confirmation.nil?)
+  end
 end
