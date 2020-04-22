abbf02d49e4282730a8ac0d3128955d462220120
Rookie <1041743913@qq.com>
2020-04-22 10:45:50 +0800

    add index management

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
 class Category < ApplicationRecord
 
+    validates :title, presence: {message: "Title can not be blank"}
+
     has_ancestry
     
     has_many :products, dependent: :destroy
+
+    before_validation :correct_ancestry
+
+
+    private
+    def correct_ancestry
+      self.ancestry = nil if self.ancestry.blank?
+    end
 end
